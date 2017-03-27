53d4d5fdb5b6430ccfc24f2c4dd992a0619cde4a
Eugene Koval <eugene@yola.com>
2017-03-27 17:05:20 +0300

    Wording

--- sitewit/services.py
+++ sitewit/services.py
@@ -377,7 +377,7 @@ class SitewitService(HTTPServiceClient):
 
         Args:
             account_token (str): account token.
-            campaign_id (str): campaign to refund.
+            campaign_id (str): campaign to cancel/refund.
             immediate (boolean): cancel immediately or wait till the billing
             period ends.
 
