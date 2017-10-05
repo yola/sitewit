f9a77ccfa69c5493cc82ecb8e5a851fb0223eb43
Volodymyr Avramuk <vavramuk@gmail.com>
2017-10-05 12:25:59 +0300

    There might be cases when budget and refill amount aren't the same - allow to pass them independently.

--- sitewit/services.py
+++ sitewit/services.py
@@ -351,25 +351,25 @@ class SitewitService(HTTPServiceClient):
             headers=self._get_account_auth_header(account_token)).json()
 
     def refill_search_campaign_subscription(
-            self, account_token, campaign_id, refill_amount, currency,
+            self, account_token, campaign_id, refill_amount, budget, currency,
             next_billing_time=None):
         return self._refill_campaign_subscription(
             CampaignTypes.SEARCH, account_token, campaign_id, refill_amount,
-            currency, next_billing_time)
+            budget, currency, next_billing_time)
 
     def refill_display_campaign_subscription(
-            self, account_token, campaign_id, refill_amount, currency,
+            self, account_token, campaign_id, refill_amount, budget, currency,
             next_billing_time=None):
         return self._refill_campaign_subscription(
             CampaignTypes.DISPLAY, account_token, campaign_id, refill_amount,
-            currency, next_billing_time)
+            budget, currency, next_billing_time)
 
     def _refill_campaign_subscription(
             self, campaign_type, account_token, campaign_id, refill_amount,
-            currency, next_billing_time):
+            budget, currency, next_billing_time):
 
         data = {
-            'budget': refill_amount,
+            'budget': budget,
             'campaignId': campaign_id,
             'chargedSpend': refill_amount,
             'currency': currency,
