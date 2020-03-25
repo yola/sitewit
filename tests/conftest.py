1e2fd7ea741ba736ba8f8f7800b957a2dbee2db9
kosmos342 <kosmos342@gmail.com>
2020-03-25 22:36:23 +0200

    Trying to deal with parallel tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,14 @@
 import os
+import time
+
+import pytest
 
 collect_ignore = []
-if not os.environ.get('INTEGRATION_TESTS'):
+if os.environ.get('INTEGRATION_TESTS'):
+    @pytest.fixture(scope='function', autouse=True)
+    def set_up():
+        time.sleep(0.5)
+else:
     collect_ignore.extend([
         'accounts/test_integration.py',
         'campaigns/test_integration.py',
